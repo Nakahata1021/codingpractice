90ff5aefe1d1dfe9d853bf077b0f329802600312
Nakahata1021 <j.nakahata.cingroup2012@gmail.com>
2024-12-12 15:44:52 +0900

    課題修正

---
+++ coding/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const items = document.querySelectorAll(".fade-in");
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    }, { threshold: 0.1 });
+
+    items.forEach(item => observer.observe(item));
+});
