849254034f546e1b6f9b72e8e90f99337b8672a9
Nakahata1021 <j.nakahata.cingroup2012@gmail.com>
2024-12-17 00:55:57 +0900

    課題修正その3

--- coding/script.js
+++ coding/script.js
@@ -11,3 +11,30 @@ document.addEventListener("DOMContentLoaded", () => {
 
     items.forEach(item => observer.observe(item));
 });
+
+document.querySelectorAll('.accordion-collapse').forEach((accordion) => {
+    accordion.addEventListener('shown.bs.collapse', (event) => {
+        const items = event.target.querySelectorAll('li');
+        items.forEach((item, index) => {
+            setTimeout(() => {
+                item.classList.add('animate');
+            }, index * 200); // 各行200msずつ遅らせる
+        });
+    });
+
+    accordion.addEventListener('hide.bs.collapse', (event) => {
+        const items = event.target.querySelectorAll('li');
+        items.forEach((item) => {
+            item.classList.remove('animate');
+        });
+    });
+});
+
+document.querySelectorAll('.navbar-toggler').forEach(button => {
+    button.addEventListener('click', () => {
+        const navbarContent = document.getElementById('navbarContent');
+        if (navbarContent.classList.contains('show')) {
+            navbarContent.classList.add('show');
+        }
+    });
+});
